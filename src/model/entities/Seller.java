f5ef2bb59fe53fc6412ba00bdcaebe7043207fdb
Gustavo Maia <ms.gustavo@outlook.com>
2025-02-23 18:00:39 -0300

    departmentDao and sellerDao interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -2,6 +2,7 @@ package model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 public class Seller implements Serializable {
 
@@ -12,12 +13,12 @@ public class Seller implements Serializable {
 	private String email;
 	private Date birthdate;
 	private Double baseSalary;
-	
+
 	private Department department;
 
 	public Seller() {
 	}
-	
+
 	public Seller(Integer id, String name, String email, Date birthdate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
@@ -26,6 +27,76 @@ public class Seller implements Serializable {
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
-	
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthdate=" + birthdate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+
 }
